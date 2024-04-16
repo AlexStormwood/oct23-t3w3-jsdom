170c7a5a90b282bef5cc8e16ee058184362ff068
Alex H <alex.holder@bigfootds.com>
2024-04-16 20:46:14 +1000

    feat: implemented JS colour schemes for CSS variables

---
+++ themeControl.js
@@ -0,0 +1,21 @@
+const themes = {
+	light: {
+		background: "white",
+		color: "darkgrey"
+	},
+	dark: {
+		background: "darkgrey",
+		color: "#ffffff"
+	}
+};
+
+function changeCssTheme(themeName){
+	// themes[themeName].variable
+	for (const variable in themes[themeName]){
+		document.documentElement.style.setProperty(`--${variable}`, themes[themeName][variable]);
+		console.log("Updated CSS variable --" + variable);
+
+	}
+}
+
+changeCssTheme("dark");
