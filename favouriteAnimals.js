2eb92ce111bd504ba84dd4848a3e37f5fd60163f
Alex H <alex.holder@bigfootds.com>
2024-04-18 20:39:47 +1000

    feat: form hints implemented

--- favouriteAnimals.js
+++ favouriteAnimals.js
@@ -94,6 +94,11 @@ function addAnimalToList(event, targetInputId){
 	let foundInputFieldValue = targetInputField.value;
 	console.log("Input field value to add to list is: " + foundInputFieldValue);
 
+
+	// 2.5 Validate for empty and duplicate data in the input field
+	// array.filter or Set to check for duplicate
+	// checking for empty string 
+
 	// 3. Push the text value into the animals array 
 	animals.push(foundInputFieldValue);
 
@@ -108,6 +113,42 @@ fakeFormButton.addEventListener("click", (event) => {addAnimalToList(event, "fak
 
 let realFormButton = document.getElementById("realform-submit");
 realFormButton.addEventListener("click", (event) => {addAnimalToList(event, "realform-addAnimal")});
+
+
+
+
+function inputHelperReveal(targetElementId){
+	let hintElement = document.getElementById(targetElementId);
+	hintElement.style.display = "inherit";
+}
+
+function inputHelperHide(targetElementId){
+	let hintElement = document.getElementById(targetElementId);
+	hintElement.style.display = "none";
+}
+
+let realFormInput = document.getElementById("realform-addAnimal");
+realFormInput.addEventListener("focusin", () => {inputHelperReveal("realform-hint")});
+realFormInput.addEventListener("focusout", () => {inputHelperHide("realform-hint")});
+inputHelperHide("realform-hint");
+
+
+let fakeFormInput = document.getElementById("fakeform-addAnimal");
+fakeFormInput.addEventListener("focusin", () => {inputHelperReveal("fakeform-hint")});
+fakeFormInput.addEventListener("focusout", () => {inputHelperHide("fakeform-hint")});
+inputHelperHide("fakeform-hint");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
